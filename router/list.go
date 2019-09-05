0dd0ac415a32fdcfe8d6091fdc517cebbfb5a842
HRDVV <hrd.candy.dev@gmail.com>
2019-09-05 19:13:43 +0800

    更新

--- router/list.go
+++ router/list.go
@@ -2,13 +2,11 @@ package router
 
 import (
 	c "covernote-backend/handle"
-	"covernote-backend/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func ListGroup(r gin.IRouter) {
 	list := r.Group("/list")
-	list.Use(middleware.AuthLogin())
 	{
 		list.GET("/get", c.GetListAll)
 		//list.POST("/add", c.AddList)
